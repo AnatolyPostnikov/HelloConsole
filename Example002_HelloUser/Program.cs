827821bf3e2d726af84a8598f8ca542c3e14041f
Anatoly Postnikov <4158888@list.ru>
2023-05-26 22:41:09 +0300

    Программа с приветствием !!!!!

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа с приветствием
+Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
+Console.WriteLine("Я тебя люблю!!!");
