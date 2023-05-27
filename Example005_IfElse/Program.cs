374b3784f1947daf5cb0ec3fd71b726472c300bb
Anatoly Postnikov <4158888@list.ru>
2023-05-27 15:28:22 +0300

    Разные приветствия if else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алена")
+{
+Console.WriteLine("Привет, самый любимый котенок!!!!!");
+}
+else
+{
+    Console.WriteLine("Добрый день,");
+    Console.WriteLine(username);
+}
